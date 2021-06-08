0a7b7e97f187a40198cfae57c13998d0aa049a69
Jannik Niemer <niemer.j@gmail.com>
2021-06-08 09:50:21 +0200

    Changed structure from function-library to class

--- src/SPL06-007.cpp
+++ src/SPL06-007.cpp
@@ -3,13 +3,8 @@
 
 uint8_t SPL_CHIP_ADDRESS = 0x76;
 
-void SPL_init(uint8_t spl_address)
+void SPL06::init()
 {
-	if(spl_address == 0x77)
-		SPL_CHIP_ADDRESS = 0x77;
-		
-	// ---- Oversampling of >8x for temperature or pressuse requires FIFO operational mode which is not implemented ---
-	// ---- Use rates of 8x or less until feature is implemented ---
 	i2c_eeprom_write_uint8_t(SPL_CHIP_ADDRESS, 0X06, 0x03);	// Pressure 8x oversampling
 
 	i2c_eeprom_write_uint8_t(SPL_CHIP_ADDRESS, 0X07, 0X83);	// Temperature 8x oversampling
@@ -19,44 +14,44 @@ void SPL_init(uint8_t spl_address)
 	i2c_eeprom_write_uint8_t(SPL_CHIP_ADDRESS, 0X09, 0X00);	// FIFO Pressure measurement  
 }
 
-uint8_t get_spl_id()
+uint8_t SPL06::get_spl_id()
 {
 	return i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0x0D);	
 }
 
-uint8_t get_spl_prs_cfg()
+uint8_t SPL06::get_spl_prs_cfg()
 {
 	return i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0x06);
 }
 
-uint8_t get_spl_tmp_cfg()
+uint8_t SPL06::get_spl_tmp_cfg()
 {
 	return i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0x07);
 }
 
-uint8_t get_spl_meas_cfg()
+uint8_t SPL06::get_spl_meas_cfg()
 {
 	return i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0x08);
 }
 
-uint8_t get_spl_cfg_reg()
+uint8_t SPL06::get_spl_cfg_reg()
 {
 	return i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0x09);
 }
 
-uint8_t get_spl_int_sts()
+uint8_t SPL06::get_spl_int_sts()
 {
 	return i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0x0A);
 }
 
-uint8_t get_spl_fifo_sts()
+uint8_t SPL06::get_spl_fifo_sts()
 {
 	return i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0x0B);
 }
 
 
 
-double get_altitude(double pressure, double seaLevelhPa) {
+double SPL06::get_altitude(double pressure, double seaLevelhPa) {
 	double altitude;
 
 	altitude = 44330 * (1.0 - pow(pressure / seaLevelhPa, 0.1903));
@@ -64,7 +59,7 @@ double get_altitude(double pressure, double seaLevelhPa) {
 	return altitude;
 }
 
-double get_altitude_f(double pressure, double seaLevelhPa)
+double SPL06::get_altitude_f(double pressure, double seaLevelhPa)
 {
 	double altitude;
 
@@ -74,14 +69,14 @@ double get_altitude_f(double pressure, double seaLevelhPa)
 }
 
 
-double get_traw_sc()
+double SPL06::get_traw_sc()
 {
 	int32_t traw = get_traw();
 	return (double(traw)/get_temperature_scale_factor());
 }
 
 
-double get_temp_c()
+double SPL06::get_temp_c()
 {
 	int16_t c0,c1;
 	c0 = get_c0();
@@ -91,7 +86,7 @@ double get_temp_c()
 }
 
 
-double get_temp_f()
+double SPL06::get_temp_f()
 {
 	int16_t c0,c1;
 	c0 = get_c0();
@@ -101,7 +96,7 @@ double get_temp_f()
 }
 
 
-double get_temperature_scale_factor()
+double SPL06::get_temperature_scale_factor()
 {
   
   double k;
@@ -120,35 +115,35 @@ double get_temperature_scale_factor()
   switch (tmp_Byte) 
   {
     case 0B000:
-      k = 524288.0d;
+      k = 524288.0;
     break;
 
     case 0B001:
-      k = 1572864.0d;
+      k = 1572864.0;
     break;
 
     case 0B010:
-      k = 3670016.0d;
+      k = 3670016.0;
     break;
 
     case 0B011:
-      k = 7864320.0d;
+      k = 7864320.0;
     break;
 
     case 0B100:
-      k = 253952.0d;
+      k = 253952.0;
     break;
 
     case 0B101:
-      k = 516096.0d;
+      k = 516096.0;
     break;
 
     case 0B110:
-      k = 1040384.0d;
+      k = 1040384.0;
     break;
 
     case 0B111:
-      k = 2088960.0d;
+      k = 2088960.0;
     break;
   }
 
@@ -156,7 +151,7 @@ double get_temperature_scale_factor()
 }
 
 
-int32_t get_traw()
+int32_t SPL06::get_traw()
 {
   int32_t tmp;
   uint8_t tmp_MSB,tmp_LSB,tmp_XLSB;
@@ -177,13 +172,13 @@ int32_t get_traw()
   return tmp;
 }
 
-double get_praw_sc()
+double SPL06::get_praw_sc()
 {
 	int32_t praw = get_praw();
 	return (double(praw)/get_pressure_scale_factor());
 }
 
-double get_pcomp()
+double SPL06::get_pcomp()
 {
 	int32_t c00,c10;
 	int16_t c01,c11,c20,c21,c30;
@@ -199,7 +194,7 @@ double get_pcomp()
 	return double(c00) + praw_sc * (double(c10) + praw_sc * (double(c20) + praw_sc * double(c30))) + traw_sc * double(c01) + traw_sc * praw_sc * ( double(c11) + praw_sc * double(c21));
 }
 
-double get_pressure()
+double SPL06::get_pressure()
 {
 	double pcomp = get_pcomp();
 	return pcomp / 100; // convert to mb
@@ -207,7 +202,7 @@ double get_pressure()
 
 
 
-double get_pressure_scale_factor()
+double SPL06::get_pressure_scale_factor()
 {
 	double k;
 
@@ -220,35 +215,35 @@ double get_pressure_scale_factor()
 	switch (tmp_Byte) // oversampling rate
 	{
 		case 0B000:
-			k = 524288.0d;
+			k = 524288.0;
 		break;
 
 		case 0B001:
-			k = 1572864.0d;
+			k = 1572864.0;
 		break;
 
 		case 0B010:
-			k = 3670016.0d;
+			k = 3670016.0;
 		break;
 
 		case 0B011:
-			k = 7864320.0d;
+			k = 7864320.0;
 		break;
 
 		case 0B100:
-			k = 253952.0d;
+			k = 253952.0;
 		break;
 
 		case 0B101:
-			k = 516096.0d;
+			k = 516096.0;
 		break;
 
 		case 0B110:
-			k = 1040384.0d;
+			k = 1040384.0;
 		break;
 
 		case 0B111:
-			k = 2088960.0d;
+			k = 2088960.0;
 		break;
 	}
 
@@ -258,7 +253,7 @@ double get_pressure_scale_factor()
 
 
 
-int32_t get_praw()
+int32_t SPL06::get_praw()
 {
   int32_t tmp;
   uint8_t tmp_MSB,tmp_LSB,tmp_XLSB;
@@ -283,7 +278,7 @@ int32_t get_praw()
   return tmp;
 }
 
-int16_t get_c0()
+int16_t SPL06::get_c0()
 {
   int16_t tmp; 
   uint8_t tmp_MSB,tmp_LSB;
@@ -305,7 +300,7 @@ int16_t get_c0()
 }
 
 
-int16_t get_c1()
+int16_t SPL06::get_c1()
 {
   int16_t tmp; 
   uint8_t tmp_MSB,tmp_LSB;
@@ -325,9 +320,8 @@ int16_t get_c1()
   return tmp;
 }
 
-int32_t get_c00()
+int32_t SPL06::get_c00()
 {
-  /*
   int32_t tmp; 
   uint8_t tmp_MSB,tmp_LSB,tmp_XLSB;
   
@@ -350,20 +344,10 @@ int32_t get_c00()
     
 
   return tmp;
-  */
-  int32_t tmp;
-  uint8_t tmp_MSB,tmp_LSB,tmp_XLSB;
-
-  tmp_MSB = i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0X13);
-  tmp_LSB = i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0X14);
-  tmp_XLSB = i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0X15);
-  tmp = (tmp_MSB & 0x80 ? 0xFFF00000 : 0) | ((uint32_t)tmp_MSB << 12) | ((uint32_t)tmp_LSB << 4) | (((uint32_t)tmp_XLSB & 0xF0) >> 4);
-  return tmp;
 }
 
-int32_t get_c10()
+int32_t SPL06::get_c10()
 {
-  /*
   int32_t tmp; 
   uint8_t tmp_MSB,tmp_LSB,tmp_XLSB;
   
@@ -387,21 +371,11 @@ int32_t get_c10()
     tmp = tmp | 0XFFF00000; // Set left bits to one for 2's complement conversion of negitive number
 
   return tmp;
-  */
-  
-  int32_t tmp;
-  uint8_t tmp_MSB,tmp_LSB,tmp_XLSB;
-
-  tmp_MSB = i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0X15); // 4 bits
-  tmp_LSB = i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0X16); // 8 bits
-  tmp_XLSB = i2c_eeprom_read_uint8_t(SPL_CHIP_ADDRESS, 0X17); // 8 bits
-  tmp = (tmp_MSB & 0x8 ? 0xFFF00000 : 0) | (((uint32_t)tmp_MSB & 0x0F) << 16) | ((uint32_t)tmp_LSB << 8) | (uint32_t)tmp_XLSB;
-  return tmp;
 }
 
 
 
-int16_t get_c01()
+int16_t SPL06::get_c01()
 {
   int16_t tmp; 
   uint8_t tmp_MSB,tmp_LSB;
@@ -413,7 +387,7 @@ int16_t get_c01()
   return tmp;
 }
 
-int16_t get_c11()
+int16_t SPL06::get_c11()
 {
   int16_t tmp; 
   uint8_t tmp_MSB,tmp_LSB;
@@ -425,7 +399,7 @@ int16_t get_c11()
   return tmp;
 }
 
-int16_t get_c20()
+int16_t SPL06::get_c20()
 {
   int16_t tmp; 
   uint8_t tmp_MSB,tmp_LSB;
@@ -438,7 +412,7 @@ int16_t get_c20()
   return tmp;
 }
 
-int16_t get_c21()
+int16_t SPL06::get_c21()
 {
   int16_t tmp; 
   uint8_t tmp_MSB,tmp_LSB;
@@ -450,7 +424,7 @@ int16_t get_c21()
   return tmp;
 }
 
-int16_t get_c30()
+int16_t SPL06::get_c30()
 {
   int16_t tmp; 
   uint8_t tmp_MSB,tmp_LSB;
@@ -464,7 +438,7 @@ int16_t get_c30()
   Serial.println(tmp);
 }
 
-void i2c_eeprom_write_uint8_t(  uint8_t deviceaddress, uint8_t eeaddress, uint8_t data ) 
+void SPL06::i2c_eeprom_write_uint8_t(  uint8_t deviceaddress, uint8_t eeaddress, uint8_t data ) 
 {
     uint8_t rdata = data;
     delay(5); // Make sure to delay log enough for EEPROM I2C refresh time
@@ -476,14 +450,14 @@ void i2c_eeprom_write_uint8_t(  uint8_t deviceaddress, uint8_t eeaddress, uint8_
 
 
 
-uint8_t i2c_eeprom_read_uint8_t(  uint8_t deviceaddress, uint8_t eeaddress ) 
+uint8_t SPL06::i2c_eeprom_read_uint8_t(  uint8_t deviceaddress, uint8_t eeaddress ) 
 {
     uint8_t rdata = 0xFF;
     Wire.beginTransmission(deviceaddress);
     Wire.write(eeaddress); 
     Wire.endTransmission(false); // false to not release the line
     
-    Wire.requestFrom(deviceaddress,1);
+    Wire.requestFrom(deviceaddress,(uint8_t)1);
     if (Wire.available()) rdata = Wire.read();
     return rdata;
 }
--- src/SPL06-007.h
+++ src/SPL06-007.h
@@ -1,40 +1,43 @@
 #include "Arduino.h"
 
-void SPL_init(uint8_t spl_address=0x76);
+class SPL06 {
+    private:
+        static uint8_t get_spl_id();		// Get ID Register 		0x0D
+        static uint8_t get_spl_prs_cfg();	// Get PRS_CFG Register	0x06
+        static uint8_t get_spl_tmp_cfg();	// Get TMP_CFG Register	0x07
+        static uint8_t get_spl_meas_cfg();	// Get MEAS_CFG Register	0x08
+        static uint8_t get_spl_cfg_reg();	// Get CFG_REG Register	0x09
+        static uint8_t get_spl_int_sts();	// Get INT_STS Register	0x0A
+        static uint8_t get_spl_fifo_sts();	// Get FIFO_STS Register	0x0B
 
-uint8_t get_spl_id();		// Get ID Register 		0x0D
-uint8_t get_spl_prs_cfg();	// Get PRS_CFG Register	0x06
-uint8_t get_spl_tmp_cfg();	// Get TMP_CFG Register	0x07
-uint8_t get_spl_meas_cfg();	// Get MEAS_CFG Register	0x08
-uint8_t get_spl_cfg_reg();	// Get CFG_REG Register	0x09
-uint8_t get_spl_int_sts();	// Get INT_STS Register	0x0A
-uint8_t get_spl_fifo_sts();	// Get FIFO_STS Register	0x0B
+        double get_altitude(double pressure, double seaLevelhPa);	// get altitude in meters
+        double get_altitude_f(double pressure, double seaLevelhPa);	// get altitude in feet
 
-double get_altitude(double pressure, double seaLevelhPa);	// get altitude in meters
-double get_altitude_f(double pressure, double seaLevelhPa);	// get altitude in feet
+        static int16_t get_c0();
+        static int16_t get_c1();
+        static int32_t get_c00();
+        static int32_t get_c10();
+        static int16_t get_c01();
+        static int16_t get_c11();
+        static int16_t get_c20();
+        static int16_t get_c21();
+        static int16_t get_c30();
 
-int32_t get_traw();
-double get_traw_sc();
-double get_temp_c();
-double get_temp_f();
-double get_temperature_scale_factor();
+        static void i2c_eeprom_write_uint8_t(  uint8_t deviceaddress, uint8_t eeaddress, uint8_t data );
+        static uint8_t i2c_eeprom_read_uint8_t(  uint8_t deviceaddress, uint8_t eeaddress );
+    public:
+        virtual void init();
 
-int32_t get_praw();
-double get_praw_sc();
-double get_pcomp();
-double get_pressure_scale_factor();
-double get_pressure();
+        virtual int32_t get_traw();
+        virtual double get_traw_sc();
+        virtual double get_temp_c();
+        virtual double get_temp_f();
+        virtual double get_temperature_scale_factor();
 
-int16_t get_c0();
-int16_t get_c1();
-int32_t get_c00();
-int32_t get_c10();
-int16_t get_c01();
-int16_t get_c11();
-int16_t get_c20();
-int16_t get_c21();
-int16_t get_c30();
-
-void i2c_eeprom_write_uint8_t(  uint8_t deviceaddress, uint8_t eeaddress, uint8_t data );
-uint8_t i2c_eeprom_read_uint8_t(  uint8_t deviceaddress, uint8_t eeaddress );
+        virtual int32_t get_praw();
+        virtual double get_praw_sc();
+        virtual double get_pcomp();
+        virtual double get_pressure_scale_factor();
+        virtual double get_pressure();
+};
 
